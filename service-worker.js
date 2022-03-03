918951c1150d1cb6d57e43f99c15e16cc8e8ae5a
Williams Sissoko <wsissoko65@gmail.com>
2022-03-02 20:54:14 -0500

    ad

--- service-worker.js
+++ service-worker.js
@@ -30,10 +30,10 @@ self.addEventListener('install', (event) => {
     )
 })
 
-self.addEventListener('activate', function (event) {
-    console.log('Claiming control');
-    return self.clients.claim();
-});
+// self.addEventListener('activate', function (event) {
+//     console.log('Claiming control');
+//     return self.clients.claim();
+// });
 
 self.addEventListener('fetch', (event) => {
     if (!(event.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
