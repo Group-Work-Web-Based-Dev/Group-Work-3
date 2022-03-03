e2d764c482ce317c3e33c01076a5313ac6038d8f
Williams Sissoko <wsissoko65@gmail.com>
2022-03-02 21:57:33 -0500

    fixed-load1

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 // register service worker
 if ('serviceWorker' in navigator) {
     let state;
-    navigator.serviceWorker.register('service-worker.js', )
+    navigator.serviceWorker.register('/service-worker.js', )
         .then(res => {
             switch (res) {
                 case res.installing:
@@ -19,7 +19,7 @@ if ('serviceWorker' in navigator) {
 
             console.log(state);
         }).catch(err => {
-        console.log(`New - [Error] loading service worker... ${err}`)
+        console.log(`Super - [Error] loading service worker... ${err}`)
     })
 } else {
     console.error(`Service worker not available in navigator`)
